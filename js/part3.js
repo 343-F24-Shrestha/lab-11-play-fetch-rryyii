b3f2b2effd56c30554a2b1109f7bd003b71cf877
rryyii <ryanyi455@gmail.com>
2024-11-22 09:23:07 -0500

    Finished part 3

--- js/part3.js
+++ js/part3.js
@@ -2,14 +2,21 @@ const output1 = document.getElementById('output-1');
 const output2 = document.getElementById('output-2');
 
 document.getElementById('api-1-btn').addEventListener('click', async () => {
-    // Make a request to your first API here. Put the response's data in output-1.
-    // If your response has no body, put the status code in output-1.
-    // TODO
+    const response = await fetch('https://www.theaudiodb.com/api/v1/json/2/album.php?i=112024');
+    const data = await response.json();
+    output1.textContent = JSON.stringify(data, null, 2);
 });
 
+
 document.getElementById('api-2-btn').addEventListener('click', async () => {
-    // Make a request to your second API here. Put the response's data in output-2.
-    // If your response has no body, put the status code in output-2.
-    
-    // TODO
+    const key = '19a0219f7af2d64ef945a24702924a8b';
+    const user = 'gaesekki';
+    const response = await fetch(
+        `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=${user}&api_key=${key}&format=json`,
+        {
+            method: 'GET',
+        }
+    );
+    const data = await response.json();
+    output2.textContent = JSON.stringify(data, null, 2);
 });
